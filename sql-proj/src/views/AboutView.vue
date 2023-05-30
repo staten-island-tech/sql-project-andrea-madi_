9243b25b1498d03d9ca8ded4de217fcb4bdfc613
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-05-30 13:52:18 -0400

    mmm

--- sql-proj/src/views/AboutView.vue
+++ sql-proj/src/views/AboutView.vue
@@ -1,15 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+const msg = ref(' ' + 'player')
+const msg2 = ref('')
+function storeJs() {}
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>{{ 'welcome' + ' ' + msg }}</h1>
+  input username:
+  <input v-model="msg" />
+  <button onclick="storeJs()">enter</button>
+  password:
+  <input v-model="msg2" />
+  <button onclick="storeJs()">enter</button>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style scoped>
+body {
+  background-color: white;
 }
 </style>
