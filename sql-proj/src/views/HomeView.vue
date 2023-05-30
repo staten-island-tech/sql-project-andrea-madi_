fbef5408dcbe0bd76d8fdd0efd496261f691473d
andreaguo <andrea.guo2007@gmail.com>
2023-05-30 13:49:20 -0400

    asdfadsf

--- sql-proj/src/views/HomeView.vue
+++ sql-proj/src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>account login</h1>
+  </div>
 </template>
+
+<style scoped>
+body{
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+}
+
+h1{
+  display: flex;
+  text-align: center;
+  margin: 3rem;
+}
+
+.container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: thistle;
+  margin: 3rem;
+  height: 8rem;  
+}
+</style>
