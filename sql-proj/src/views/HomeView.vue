87351a7125470e0d9b298d4da5fbcb8a72c613b2
madisonmeccia <113203246+madisonmeccia@users.noreply.github.com>
2023-06-01 17:35:10 -0400

    n

--- sql-proj/src/views/HomeView.vue
+++ sql-proj/src/views/HomeView.vue
@@ -1,32 +1,44 @@
-<script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <div class="container">
-    <h1>account login</h1>
+  <div class="home">
+    <Card v-for="deck in decks" :key="deck.url" />
   </div>
 </template>
 
-<style scoped>
-body{
-  display: flex;
-  text-align: center;
-  flex-direction: column;
-}
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue';
+import Card from '../components/Card.Vue';
 
-h1{
-  display: flex;
-  text-align: center;
-  margin: 3rem;
-}
+export default {
+  name: "Home",
+};
 
-.container{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background-color: thistle;
-  margin: 3rem;
-  height: 8rem;  
-}
-</style>
+  data() {
+    return{
+       selected: "",
+ decks: [
+  {
+    type: 'aurora',
+    url: 'https://www.pngmart.com/files/6/Princess-Aurora-PNG-Picture.png'
+  },
+  {
+    type: 'cinderella',
+    url: 'https://clipart-library.com/images_k/disney-princess-silhouette-free-printables/disney-princess-silhouette-free-printables-12.png'
+  },
+  {
+    type: 'rapunzel',
+    url: 'https://www.freeiconspng.com/thumbs/rapunzel-png/image-rapunzel-png-7.png'
+  },
+  {
+    type: 'mulan',
+    url: 'https://www.nicepng.com/png/full/18-186074_fa-mulan-free-png-image-disney-princess-mulan.png'
+  },
+  {
+    type: 'jasmine',
+    url: 'https://i.pinimg.com/originals/44/7b/af/447baf7ba53755e1526beff65e5dc774.png'
+  },
+  {
+    type: 'tiana',
+    url: 'https://i.pinimg.com/originals/a0/44/51/a04451d5e5befbedf0bfeba4e0b9d2f2.png'
+  },
+  ],
+</script>
