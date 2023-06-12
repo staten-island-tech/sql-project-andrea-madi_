c7e723c8b92cf4c98e6d2ee8873cbcb3777f58a4
andreaguo <113132239+andreaguo@users.noreply.github.com>
2023-06-12 01:51:31 -0400

    account works

--- sql-proj/src/views/AccountView.vue
+++ sql-proj/src/views/AccountView.vue
@@ -1,33 +1,74 @@
 <template>
-    
+    <h1>{{ errorMsg }}</h1>
+    <div class="profile">
+        <img :src="currentUser.avatar_url" alt="">
+        <div class="content">
+            <label for="username">USERNAME:</label>
+            <h1>{{ currentUser.full_name }}</h1>
+            <label for="highscore">HIGH SCORE:</label>
+            <h1>{{ currentUser.high_score }}</h1>
+        </div>
+        
+    </div>
 </template>
 
+
+<style scoped>
+body{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.profile{
+    background-color: #6969b35d;
+    width: 80vw;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: row;
+}
+.content, label{
+    background-color: transparent;
+}
+img{
+    height: 25vh;
+    width: 25vh;
+    margin: 3rem;
+}
+h1{
+    background-color: transparent;
+    margin: 2rem;
+}
+label{
+    font-size: 2rem;
+}
+</style>
 <script>
 import { ref } from 'vue';
 import { supabase } from '../supabase';
 import { useRoute } from 'vue-router';
+import counter from '../stores/counter'
 export default{
     name: 'account',
     setup(){
-        const data = ref(null)
+        const currentUser = ref(null)
         const dataLoaded = ref(null)
         const errorMsg = ref(null)
-        const statusMsg = ref(null)
-        const route = useRoute()
 
         // get current id of user
-        const currentID = route.params.userID
+        const currentID = counter.state.user.id
 
         // get workout data
         const getData = async () => {
             try {
-                const {data: profile, error} = await supabase
+                const {data, error} = await supabase
                 .from('profiles')
-                .select('*')
+                .select()
                 .eq('id', currentID)
-            if(error) throw error
-            data.value = profile
-            dataLoaded.value = true
+                .single()
+                if(error) throw error
+                console.log(data)
+                currentUser.value = data
+                dataLoaded.value = true
             } catch (error) {
                 errorMsg.value = error.message
                 setTimeout(()=>{
@@ -38,6 +79,11 @@ export default{
         }
         getData()
 
+        return{
+            currentUser,
+            dataLoaded,
+            errorMsg
+        }
     }
 }
 </script>
